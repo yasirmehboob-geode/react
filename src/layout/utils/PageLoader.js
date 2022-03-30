df14bb6e0a95bdf2d5b24c348e5a74558d073db0
Yasir Mehboob <yasir.geode@nxvt.com>
2022-03-30 21:21:01 +0500

    change loader

--- src/layout/utils/PageLoader.js
+++ src/layout/utils/PageLoader.js
@@ -1,6 +1,6 @@
 import './PageLoader.css';
 import React from 'react';
-import pageLoading from './../../assets/img/page-loading-2.gif';
+import pageLoading from './../../assets/img/page-loading-3.gif';
 
 export function PageLoader(props) {
     return (
